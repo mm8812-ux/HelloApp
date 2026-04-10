5e7f157d491c3670efd1aeb3730638d919311df1
manjunath <mm8812@srmist.edu.in>
2026-04-10 13:59:38 +0530

    added:uc1

---
+++ src/main/java/HelloApp.java
@@ -0,0 +1,10 @@
+public class HelloApp {
+
+    /**
+     * Main method – entry point of the Java program.
+     * It prints a greeting message to the console.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
