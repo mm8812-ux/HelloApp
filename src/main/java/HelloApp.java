412f0c09b644ecea6e734590fc3f30285a36e2a4
manjunath <mm8812@srmist.edu.in>
2026-04-21 15:03:05 +0530

    added:uc7

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,26 +1,10 @@
-public class HelloApp {
-    public static void main(String[] args) {
+   }
 
-        StringBuilder namesBuilder = new StringBuilder();
-    public static void main(String[] args) {
-
-        // Check if no arguments are provided
-        // Case 1: No arguments
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-            return;
-        }
-
-        // Enhanced for loop to iterate through names
         // Case 2: Arguments present
         StringBuilder nameBuilder = new StringBuilder();
 
         // Enhanced for loop to append names
         for (String name : args) {
-            if (namesBuilder.length() > 0) {
-                namesBuilder.append(", ");
-            }
-            namesBuilder.append(name);
             nameBuilder.append(name).append(", ");
         }
 
@@ -29,10 +13,7 @@ public class HelloApp {
         if (nameBuilder.length() > 0) {
             names = nameBuilder.substring(0, nameBuilder.length() - 2);
         }
+        String names = String.join(", ", args);
 
-        // Final message
-        System.out.println("Hello, " + namesBuilder.toString() + "!");
         // Final output
         System.out.println("Hello, " + names + "!");
-    }
-}
