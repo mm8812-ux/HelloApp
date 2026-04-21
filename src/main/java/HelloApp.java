c7d8479effe5697fce36dfc79203c96348a31e78
manjunath <mm8812@srmist.edu.in>
2026-04-21 14:47:32 +0530

    added:uc5

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -2,6 +2,7 @@ public class HelloApp {
     public static void main(String[] args) {
 
         String message;
+        StringBuilder namesBuilder = new StringBuilder();
 
         // Check if arguments are provided
         if (args.length > 0) {
@@ -11,9 +12,23 @@ public class HelloApp {
         } else {
             // Default case
             message = "Hello, World!";
+        // Check if no arguments are provided
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
         }
 
         // Print the message
         System.out.println(message);
+        // Enhanced for loop to iterate through names
+        for (String name : args) {
+            if (namesBuilder.length() > 0) {
+                namesBuilder.append(", ");
+            }
+            namesBuilder.append(name);
+        }
+
+        // Final message
+        System.out.println("Hello, " + namesBuilder.toString() + "!");
     }
 }
